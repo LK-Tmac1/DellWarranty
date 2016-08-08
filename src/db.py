45c85863cc9530bd3bf5b5a5d0f1b81e918e7522
Kun Liu <liuku@us.ibm.com>
2016-08-08 10:09:10 -0400

    mysql db

--- src/db.py
+++ src/db.py
@@ -1,5 +1,7 @@
 import MySQLdb as mdb
+import yaml
 
+# /usr/local/mysql/bin
 config_path = "/Users/kunliu/Desktop/work/dell_config.yml"
 
 with open(config_path, "r") as input:
@@ -13,3 +15,5 @@ db_user = config['db_user']
 db_pwd = config['db_password']
 
 con = mdb.connect(host=host, port=db_port, user=db_user, passwd=db_pwd)
+
+
