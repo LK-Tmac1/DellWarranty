ea4d008d40130da202329c82577b496567aa9a93
Kun Liu <liukun1016@gmail.com>
2017-06-24 20:28:44 -0700

    save log to output before send email

--- src/py/main.py
+++ src/py/main.py
@@ -98,5 +98,7 @@ if __name__ == "__main__":
 	subject = subject_temp % ("[查询任务结束] ", get_current_datetime(is_date=True), svctag)
 	if logger.has_error:
 		additional_text += "\n查询程序出现错误，请等待解决。"
+	save_object_to_path(value=logger, output_path=log_output_path)
 	send_email(subject=subject, text=additional_text, config=config, cc_mode=logger.has_error or need_translation, attachment_path_L=[log_output_path, dell_asset_output_path])
 	delete_file(dell_asset_output_path)
+	delete_file(log_output_path)
